f9644fb6195da57d1b2aa3e5c2c0b95cdae82673
Van <singularity@vannio.co.uk>
2017-01-22 09:40:48 +0000

    Working text to speech. yay

--- www/js/app.js
+++ www/js/app.js
@@ -4,162 +4,162 @@ var app = {}
 // Regions that define which page to show for each beacon.
 // app.beaconRegions = [];
 app.beaconRegions = [
-	{
-		id: 'page-0',
-		uuid:'852c0828-fe67-4dd7-b8ff-52852a66851e',
-		major: 8008,
-		minor: 1337
-	},
+	// {
+	// 	id: 'page-0',
+	// 	uuid:'852c0828-fe67-4dd7-b8ff-52852a66851e',
+	// 	major: 8008,
+	// 	minor: 1337
+	// },
   {
     "id": "page-1",
     "uuid": "bbd7be4e-e642-49fd-b8ee-7516456bf2ed",
     "major": 0,
     "minor": 0
   },
-  {
-    "id": "page-2",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 5
-  },
-  {
-    "id": "page-3",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 2
-  },
-  {
-    "id": "page-4",
-    "uuid": "8aa10000-0a46-115f-d94e-5a966a3ddbb7",
-    "major": 9906,
-    "minor": 21639
-  },
-  {
-    "id": "entrance-stairs-1",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 1
-  },
-  {
-    "id": "bottom-of-entrance-stairs-2",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 2
-  },
-  {
-    "id": "ticket-hall-3",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 3
-  },
-  {
-    "id": "exit-barriers-4",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 4
-  },
-  {
-    "id": "entrance-barriers-5",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 5
-  },
-  {
-    "id": "top-of-escalator-1-6",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 6
-  },
-  {
-    "id": "top-of-escalator-2-7",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 7
-  },
-  {
-    "id": "middle-of-escalator-1-8",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 8
-  },
-  {
-    "id": "middle-of-escalator-2-9",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 9
-  },
-  {
-    "id": "bottom-of-escalator-1-10",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 10
-  },
-  {
-    "id": "bottom-of-escalator-2-11",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 11
-  },
-  {
-    "id": "walkway-bottom-of-escalators-12",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 12
-  },
-  {
-    "id": "walkway-13",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 13
-  },
-  {
-    "id": "walkway-14",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 14
-  },
-  {
-    "id": "fictional-platform-15",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 63,
-    "minor": 15
-  },
-  {
-    "id": "entrance-door-16",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 1
-  },
-  {
-    "id": "walkway-17",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 2
-  },
-  {
-    "id": "walkway-18",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 3
-  },
-  {
-    "id": "far-wall-platform-19",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 4
-  },
-  {
-    "id": "far-wall-platform-20",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 5
-  },
-  {
-    "id": "walkway-21",
-    "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
-    "major": 999,
-    "minor": 6
-  }
+  // {
+  //   "id": "page-2",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 5
+  // },
+  // {
+  //   "id": "page-3",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 2
+  // },
+  // {
+  //   "id": "page-4",
+  //   "uuid": "8aa10000-0a46-115f-d94e-5a966a3ddbb7",
+  //   "major": 9906,
+  //   "minor": 21639
+  // },
+  // {
+  //   "id": "entrance-stairs-1",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 1
+  // },
+  // {
+  //   "id": "bottom-of-entrance-stairs-2",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 2
+  // },
+  // {
+  //   "id": "ticket-hall-3",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 3
+  // },
+  // {
+  //   "id": "exit-barriers-4",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 4
+  // },
+  // {
+  //   "id": "entrance-barriers-5",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 5
+  // },
+  // {
+  //   "id": "top-of-escalator-1-6",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 6
+  // },
+  // {
+  //   "id": "top-of-escalator-2-7",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 7
+  // },
+  // {
+  //   "id": "middle-of-escalator-1-8",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 8
+  // },
+  // {
+  //   "id": "middle-of-escalator-2-9",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 9
+  // },
+  // {
+  //   "id": "bottom-of-escalator-1-10",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 10
+  // },
+  // {
+  //   "id": "bottom-of-escalator-2-11",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 11
+  // },
+  // {
+  //   "id": "walkway-bottom-of-escalators-12",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 12
+  // },
+  // {
+  //   "id": "walkway-13",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 13
+  // },
+  // {
+  //   "id": "walkway-14",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 14
+  // },
+  // {
+  //   "id": "fictional-platform-15",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 63,
+  //   "minor": 15
+  // },
+  // {
+  //   "id": "entrance-door-16",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 1
+  // },
+  // {
+  //   "id": "walkway-17",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 2
+  // },
+  // {
+  //   "id": "walkway-18",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 3
+  // },
+  // {
+  //   "id": "far-wall-platform-19",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 4
+  // },
+  // {
+  //   "id": "far-wall-platform-20",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 5
+  // },
+  // {
+  //   "id": "walkway-21",
+  //   "uuid": "3f846818-1f85-4f93-a46d-039c3056a4b1",
+  //   "major": 999,
+  //   "minor": 6
+  // }
 ]
 
 // Currently displayed page.
@@ -181,39 +181,11 @@ app.onDeviceReady = function() {
 	// has the iBeacon functions.
 	window.locationManager = cordova.plugins.locationManager
 
-	document.getElementById('messages').innerHTML = 'Device is ready';
-
-	var u = new SpeechSynthesisUtterance();
-  u.text = 'hello';
-	u.volume = 1;
-  window.speechSynthesis.speak(u);
-
-	document.getElementById('messages').innerHTML = 'Can get past speech synthesis';
-
 	// Start tracking beacons!
 	app.startScanForBeacons()
 }
 
-var sayHello = function () {
-	var u = new SpeechSynthesisUtterance();
-	u.text = 'hello';
-	u.onstart = function () {
-		document.getElementById('messages').innerHTML = 'Speech started, can you hear?';
-	}
-
-	u.onend = function () {
-		document.getElementById('messages').innerHTML = 'Speech ended, did you hear?';
-	}
-
-	u.onerror = function (e) {
-		document.getElementById('messages').innerHTML = 'There was an error:' + e.error;
-	}
-
-	window.speechSynthesis.speak(u);
-}
-
 app.startScanForBeacons = function() {
-	// console.log('startScanForBeacons')
 
 	// The delegate object contains iBeacon callback functions.
 	var delegate = new cordova.plugins.locationManager.Delegate()
@@ -290,6 +262,11 @@ app.didRangeBeaconsInRegion = function(pluginResult) {
 	}
 }
 
+app.sayWords = function (words) {
+	words = words || 'hello';
+	TTS.speak(words);
+}
+
 app.gotoPage = function(pageId) {
 	app.hidePage(app.currentPage)
 	app.showPage(pageId)
@@ -297,7 +274,9 @@ app.gotoPage = function(pageId) {
 }
 
 app.showPage = function(pageId) {
-	document.getElementById(pageId).style.display = 'block'
+	var block = document.getElementById(pageId);
+	block.style.display = 'block'
+	app.sayWords(block.innerText);
 }
 
 app.hidePage = function(pageId) {
